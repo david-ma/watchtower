dd976b890310c71e6077f9869c13b5bdbb376775
David Ma <david.ma@petermac.org>
2020-08-16 16:16:44 +1000

    Add Prototype Public files to source code

--- config.js
+++ config.js
@@ -1,3 +1,17 @@
 exports.config = {
-	"domains": ["watchtower.david-ma.net"]
+	"domains": ["watchtower.david-ma.net"],
+	"proxies": {
+		"govhack2020.david-ma.net": {
+			"filter": "screens",
+			"message": "Error, server might be down or proxy settings might be wrong. You can try visiting the server directly at: http://115.146.86.36:8080/PathOS/",
+			"host": "projects.invisionapp.com/share/MKYFUQ7QBFY#/screens",
+			"port": 443
+		},
+		"watchtower.david-ma.net": {
+			"filter": "screens",
+			"message": "Error, server might be down or proxy settings might be wrong. You can try visiting the server directly at: http://115.146.86.36:8080/PathOS/",
+			"host": "projects.invisionapp.com/share/MKYFUQ7QBFY#/screens",
+			"port": 443
+		}
+	}
 };
